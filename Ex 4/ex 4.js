88a2de6a423433bbd2c1fb80759e9e776bd76c07
Hassnmahad <hassanmahad07@gmail.com>
2026-08-17 10:49:46 -0700

    New

---
+++ Ex 4/ex 4.js
@@ -0,0 +1,23 @@
+
+
+
+function add(a,b){
+    console.log("the sum of a and b is ", + a+b);
+}
+
+add(2,4);
+add(5,20);
+add(40,45);
+add(67,88);
+
+
+// expresion
+
+const sum = function (a,b){
+    console.log("the sum of a and b is ", + a+b);
+};
+
+sum(2,4);
+sum(5,20);
+sum(40,45);
+sum(67,88);

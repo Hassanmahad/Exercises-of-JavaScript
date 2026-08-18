343f2ff62b183eaa8df599fb1608910a83a35386
Hassnmahad <hassanmahad07@gmail.com>
2026-08-18 04:09:51 -0700

    Ex 9 ayaan kuso daray

---
+++ Ex 9/ex 9.js
@@ -0,0 +1,11 @@
+
+let car = {
+    make: "2 feb 2021",
+    model: 2021,
+    year: 2021,
+    start : function(){
+        console.log("The car has started");
+    }
+};
+
+console.log(car.start());

0a2673b0ea1cf52f628ab24a6aca0635e3b49fc2
Hassnmahad <hassanmahad07@gmail.com>
2026-08-19 09:46:25 -0700

    Ex 10 ayaan kuso daray

---
+++ Ex 10/ex 10.js
@@ -0,0 +1,16 @@
+
+let Libarary = [
+{
+title : "Javascript",
+Author : "Hasey",
+year : 2020 ,
+},
+{
+title : "Html",
+Author : "Naima",
+year : 2026 ,
+}
+]
+
+console.log(Libarary[0].title);
+console.log(Libarary[1].Author);

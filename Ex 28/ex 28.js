384009d5d6da6ba03a81b79cd28930aab2d7ec30
Hassnmahad <hassanmahad07@gmail.com>
2026-08-23 11:37:14 -0700

    Ex 28 ayaan kuso daray

---
+++ Ex 28/ex 28.js
@@ -0,0 +1,29 @@
+function fetchUserData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = true;
+
+            if (success) {
+                resolve({ id: 1, name: "John Doe" });
+            } else {
+                reject("Failed to fetch user data");
+            }
+        }, 2000);
+    });
+}
+
+async function displayData() {
+    try{
+const user = await fetchUserData();
+console.log(user);
+    }catch(error) {
+        console.log("error");
+
+
+    }
+}
+
+
+displayData()
+    // .then(data => console.log("User Data:", data))
+    // .catch(error => console.error("Error:", error));

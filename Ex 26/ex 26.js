87a2c8ef6e0de832a0a7833343c060ebfe304182
Hassnmahad <hassanmahad07@gmail.com>
2026-08-23 10:35:11 -0700

    Ex 26 ayaan kuso daray

---
+++ Ex 26/ex 26.js
@@ -0,0 +1,17 @@
+
+function deley(){
+alert("Aniga iga gudub horta");
+}
+deley();
+
+console.log("Ka dib ayaan arkeysaa kan");
+
+
+
+console.log("1");
+
+setTimeout(() => {
+    console.log("2");
+}, 2000);
+
+console.log("3");

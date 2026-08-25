20560b68cf2fca177e12728c9fdbdf7c7247b05b
Hassnmahad <hassanmahad07@gmail.com>
2026-08-25 10:59:37 -0700

    Ex 33 ayaan kuso daray

---
+++ Ex 33/Ex 33.js
@@ -0,0 +1,14 @@
+
+
+
+const cinwan = document.querySelector('#cinwan');
+console.log(cinwan);
+const qoraal = document.querySelectorAll('.qoraal');
+console.log(qoraal);
+const btn = document.querySelectorAll('#btn');
+console.log(btn);
+
+function changeContent(){
+    cinwan.textContent = "Waan badalay hada" ;
+}
+

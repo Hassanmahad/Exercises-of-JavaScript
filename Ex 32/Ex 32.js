e91780e5bc04093641fbe53ad85f4b0b6b328ed6
Hassnmahad <hassanmahad07@gmail.com>
2026-08-25 10:24:27 -0700

    Ex 32 ayaan kuso daray

---
+++ Ex 32/Ex 32.js
@@ -0,0 +1,9 @@
+
+
+
+const cinwan = document.querySelector('#cinwan');
+console.log(cinwan);
+const qoraal = document.querySelectorAll('.qoraal');
+console.log(qoraal);
+const btn = document.querySelectorAll('#btn');
+console.log(btn);

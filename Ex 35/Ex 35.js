6069c3c8d087dad3c3a518547a703aa6712187e7
Hassnmahad <hassanmahad07@gmail.com>
2026-08-26 04:37:10 -0700

    Ex 35 ayaan kuso daray

---
+++ Ex 35/Ex 35.js
@@ -0,0 +1,10 @@
+
+const List = document.querySelector('.list');
+
+
+function ChangeAtribute(){
+const img = document.querySelector('#img');
+const url = prompt('Gali Sawirka URL-kiisa');
+img.setAttribute('src',url);
+}
+
